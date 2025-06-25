8ef16817a2815d41d65e462bc20fa744cc6529fd
Borrispooman <freddyroberts69420@gmail.com>
2025-06-25 22:09:18 +1000

    gameBoard module IIFE

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("hello world")
+const gameBoard = (function() {
+    const board = [null, null, null, null, null, null, null, null, null];
+
+    const clearBoard =function(){
+        boardoard = [null, null, null, null, null, null, null, null, null]
+    };
+
+    return ({board: board, sayHello: sayHello, clearBoard: clearBoard});
+
+})();;
+
+
+function createPlayer(){
+    const greet = function (){
+        console.log("hello world")
+    }
+
+    const setMarker = function(){}
+
+    return ({greet})
+
+} 
+
+function createGameControls() {
+    const clearBoard =function(){
+        gameBoard.board = [null, null, null, null, null, null, null, null, null]
+    };
+
+    const playRound = function(){
+        createPlayer(player1, player2)
+
+    }
+
+    return {clearBoard}
+    
+}
+
+
+
+
+
